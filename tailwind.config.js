d9f051cf33298cce2179e7fa349badac43406924
Sijal Manandhar <sijal.m06@gmail.com>
2023-06-22 21:44:03 +0545

    background color & pattern added, colors in "tailwind.config.js" added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+      },
+    },
   },
   plugins: [],
 };
